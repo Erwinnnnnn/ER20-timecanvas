458697de79414707ca569b7e4ba88ac3b4d26b32
erwin <erwin@ambrero.nl>
2020-02-10 17:36:03 +0100

    Fixes

--- canvas.js
+++ canvas.js
@@ -10,6 +10,7 @@ Array.prototype.diff = function(a) {
     return this.filter(function(i) {return a.indexOf(i) < 0;});
 };
 
+
 // canvas
 const canvas = document.getElementById('timeCanvas');
 const ctx = canvas.getContext("2d");
@@ -17,15 +18,15 @@ let start = null;
 let animatePositions = [];
 let numberPositions = [];
 
-const pointDistance = 12;
+const pointDistance = 15;
 const margin = 40;
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
-const circleFill = 'rgba(255,255,255,0.4)';
+const circleFill = 'rgba(255,255,255,0.2)';
 const circleFillActive = 'rgba(255,255,255,1)';
-const minCircleSize = 0.1;
-const maxCircleSize = 4;
-const minCircleSizeActive = 4;
+const minCircleSize = 0.01;
+const maxCircleSize = 5;
+const minCircleSizeActive = 3;
 const maxCircleSizeActive = 4;
 const numberOfPointsX = Math.floor(((canvasWidth  - (margin * 2)) / pointDistance));
 const numberOfPointsY = Math.floor(((canvasHeight  - (margin * 2)) / pointDistance));
@@ -33,10 +34,11 @@ const itemWidth = 8;
 const itemSpacing = 0;
 const growSpeed = 120;
 const startingSpeed = 150;
-const movementArc = 0.04
-const targetDate = new Date(2020,2,9,11,0,0,0);
+const movementArc = 0.04;
+const targetDate = new Date(2020, 1, 11, 0, 0, 0, 0);
 const totalDigits = 8;
 
+let currentTime = '';
 let digitLocations = [];
 let currentTimeArray = [];
 let currentDigitDots = [];
@@ -135,22 +137,33 @@ const numbers = [[
 ];
 
 function calculateSeconds() {
-    return (Math.round((new Date().getTime() - targetDate.getTime()) / 1000) * -1).toString();
+     let seconds = (Math.round((new Date().getTime() - targetDate.getTime()) / 1000) * -1).toString();
+    return seconds;
 }
 
 function init() {
     clear();
-    canvas.width = 1000;
-    canvas.height = 380;
+    canvas.width = 1100;
+    canvas.height = 450;
     setGridPositions();
-    setDigitLocations();
     window.cancelAnimationFrame(update);
     window.requestAnimationFrame(update);
-    currentDigitDots = getDotPositionsForDigits();
-    console.log(currentDigitDots);
+    digitLocations = setDigitLocations();
+    setupCurrentTimer();
     initDots();
 }
 init();
+
+function setupCurrentTimer() {
+    currentTime = calculateSeconds();
+    currentTimeArray = getTimeArray(currentTime);
+    for (let t = 0; t < currentTimeArray.length; t++) {
+        let positionsForDigit = getDotPositionsForDigits(currentTimeArray[t] - 1, digitLocations[t]);
+        for (let t = 0; t < positionsForDigit.length; t++) {
+            currentDigitDots.push(positionsForDigit[t]);
+        }
+    }
+}
 
 function setGridPositions() {
     let currentX = margin;
@@ -166,23 +179,26 @@ function setGridPositions() {
 }
 
 function setDigitLocations() {
-    let xPosition = Math.ceil((numberOfPointsX - ((8 * itemWidth) + (7 * itemSpacing))) / 2);
+    let xPosition = Math.ceil((numberOfPointsX - ((8 * itemWidth) + (7 * itemSpacing))) / 2) - 1;
     for(let i = 0; i < totalDigits; i++) {
-        digitLocations.push(numberOfPointsX * 8 + xPosition);
+        digitLocations.push(numberOfPointsX * ((numberOfPointsY - itemWidth) / 2) + xPosition);
         xPosition += itemSpacing + itemWidth;
     }
+    return digitLocations;
 }
 
-function getDotPositionsForDigits() {
-
+function getDotPositionsForDigits(digit, loc) {
     if(digit === -1){ digit = 9; }
     let numberArray = numbers[digit];
     let iteratePos = loc;
     let currentDigitDots = [];
     for(let i = 0; i < numberArray.length; i++) {
-        currentDigitDots.push(iteratePos);
+        let nr = numberArray[i];
         for(let s = 0; s < nr.length; s++) {
             iteratePos++;
+            if(nr[s] === 1) {
+                currentDigitDots.push(iteratePos);
+            }
         }
         iteratePos += numberOfPointsX - 8;
     }
@@ -203,6 +219,12 @@ function getTimeArray(time) {
 function animate() {
     clear();
     drawDots();
+
+    let newTime = calculateSeconds();
+    if (newTime !== currentTime) {
+        currentDigitDots = [];
+        setupCurrentTimer();
+    }
 }
 
 function Dot(drawer,type,x,y,minsize,maxsize,color) {
@@ -213,35 +235,48 @@ function Dot(drawer,type,x,y,minsize,maxsize,color) {
         this.currentY = this.y;
         this.size = rnd(0, maxsize);
         this.type = type;
+        this.minSize = minsize;
         this.maxSize = rnd(minsize, maxsize);
         this.color = color;
         this.counter = 0;
-        this.startOffset = rnd(0,type === 2 ? startingSpeed : startingSpeed * 4);
+        this.startOffset = rnd(0,startingSpeed);
         this.growing = true;
         this.angle = 0;
         this.angleSpeed = rnd(0.5,3);
+        this.active = false;
+
+        this.movement = 0;
+        this.isMovingToPoint = false;
     };
 
     this.draw = function() {
 
-        if(this.type === 1) {
+        if(!this.active) {
             this.currentX = movementArc * Math.cos(this.angle * (Math.PI / 180)) + this.currentX;
             this.currentY = movementArc * Math.cos(this.angle * (Math.PI / 180)) + this.currentY;
             this.angle += this.angleSpeed;
         }
 
-        if(this.growing) {
+
+        if (this.growing) {
             this.size += (this.maxSize / growSpeed);
-        } else if(this.type === 1) {
+        } else {
             this.size -= (this.maxSize / growSpeed);
         }
+
 
         if(this.size > this.maxSize) {
             this.growing = false;
         }
 
-        if(this.size < 1 && this.type === 1) {
-            this.growing = true;
+        if(this.active) {
+            if (this.size < this.minSize) {
+                this.growing = true;
+            }
+        } else {
+            if (this.size < 1) {
+                this.growing = true;
+            }
         }
 
         if(this.counter > this.startOffset) {
@@ -256,11 +291,40 @@ function Dot(drawer,type,x,y,minsize,maxsize,color) {
     };
 
     this.setActive = function() {
-        this.color = 'rgba(255,0,0,1)';
-    }
+
+        if(!this.isMovingToPoint) {
+            this.isMovingToPoint = true;
+            this.tx = this.x - this.currentX;
+            this.ty = this.y - this.currentY;
+            this.distance = Math.abs(Math.sqrt(this.tx * this.tx + this.ty * this.ty));
+            this.moveToPointSpeed = 3 / this.distance;
+            this.velX = (this.tx / this.distance) * 3;
+            this.velY = (this.ty / this.distance) * 3;
+            document.getElementById('test').innerHTML = this.moveToPointSpeed;
+        }
+
+        this.movement += 0.02;
+        if(this.isMovingToPoint) {
+            if (this.currentX !== this.x || this.currentY !== this.y) {
+                this.currentX += this.velX;
+                this.currentY += this.velY;
+            } else {
+                this.isMovingToPoint = false;
+            }
+        }
+
+        this.color = circleFillActive;
+        this.maxSize = rnd(minCircleSizeActive, maxCircleSizeActive);
+        this.minSize = minCircleSizeActive;
+        this.active = true;
+    };
+
+    // https://stackoverflow.com/questions/19041434/html5-canvas-move-directly-to-point
 
     this.setInActive = function() {
         this.color = circleFill;
+        // this.maxSize = rnd(minsize, maxsize);
+        this.active = false;
     }
 }
 
@@ -275,9 +339,9 @@ function initDots() {
 function drawDots() {
     for(let dotdraw = 0; dotdraw < animatePositions.length; dotdraw++) {
         animatePositions[dotdraw].draw();
-        if(rnd(0,100) < 2) {
+        if(currentDigitDots.includes(dotdraw)) {
             animatePositions[dotdraw].setActive();
-        } else{
+        } else {
             animatePositions[dotdraw].setInActive();
         }
     }
